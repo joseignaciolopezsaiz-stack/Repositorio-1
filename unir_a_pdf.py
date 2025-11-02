7429321c2d5a8288f4869c8519be2f9acd805e5a
joseignaciolopezsaiz-stack <jose.ignacio.lopez.saiz@gmail.com>
2025-11-02 10:36:48 +0100

    Add files via upload

---
+++ unir_a_pdf.py
@@ -0,0 +1,30 @@
+
+from pypdf import PdfWriter
+
+import os
+os.chdir("/")
+os.chdir("storage/emulated/0/Documents/python/pdfs")
+print(os.getcwd())
+pdf_merger = PdfWriter()
+with open("BCF.PDF","rb") as file1:
+    pdf_merger.append(file1)
+
+with open("Lista1.PDF", 'rb') as file2:
+    pdf_merger.append(file2)
+with open("Lista2.PDF", 'rb') as file3:
+    pdf_merger.append(file3)
+
+with open("unidos.pdf", 'wb') as output_file:
+    pdf_merger.write(output_file)
+print(os.getcwd())
+
+#_________
+#Renombrar unidos.pdf --> pdf.PDF
+for f in os.listdir(os.getcwd()):
+    if f.endswith("unidos.pdf"):
+        ruta_actual = os.path.join(os.getcwd(), f)
+print(ruta_actual)
+
+ruta_nueva = os.path.join(os.getcwd(), "pdf.PDF")
+print(ruta_nueva)
+os.rename(ruta_actual, ruta_nueva)
